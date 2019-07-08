7b0b8a3b8fea3cec2c6ef54b3471e93f881bb998
transfer76 <shina76@ukr.net>
2019-07-08 17:26:23 +0300

    Add schedule for sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   devise_for :users
+
   root 'vulnerabilities#index'
 
   resources :vulnerabilities, except: :edit
+
+  mount Sidekiq::Web => '/sidekiq'
 end
--- config/schedule.rb
+++ config/schedule.rb
@@ -18,3 +18,6 @@
 # end
 
 # Learn more: http://github.com/javan/whenever
+every 10.minutes do
+  runner "VulnerabilityWorker.perform_async"
+end
