7d48fe7cb7d2f9337d1401f29b26b6ec588dfa45
transfer76 <shina76@ukr.net>
2019-07-08 12:08:08 +0300

    Add gem dotenv

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'devise'
 gem 'twitter-bootstrap-rails'
 gem 'sidekiq'
 gem 'whenever', require: false
+gem 'dotenv'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: '587',
     user_name: 'testerdevise',
-    password: '',
+    password: ENV['EMAIL_PASSWORD'],
     authentication: 'plain',
     enable_starttls_auto: true
   }
